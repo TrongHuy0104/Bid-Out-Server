13e41e6b7a2e8775fd74338bc77af8f2a71b2bd1
huy01040104 <tronghuy01040@gmail.com>
2025-02-05 08:34:12 +0700

    feat: add server api

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,26 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  if (!options.email) {
-    throw new Error("No recipients defined");
-  }
-  const transport = nodemailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    auth: {
-      user: process.env.SMPT_EMAIL,
-      pass: process.env.SMPT_PASS,
-    },
-  });
+    if (!options.email) {
+        throw new Error('No recipients defined');
+    }
+    const transport = nodemailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port: process.env.SMPT_PORT,
+        auth: {
+            user: process.env.SMPT_EMAIL,
+            pass: process.env.SMPT_PASS,
+        },
+    });
 
-  const message = {
-    from: `${process.env.SMPT_FROM_NAME} <${process.env.SMPT_FROM_EMAIL}> `,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    const message = {
+        from: `${process.env.SMPT_FROM_NAME} <${process.env.SMPT_FROM_EMAIL}> `,
+        to: options.email,
+        subject: options.subject,
+        text: options.text,
+    };
+    console.log('message', message);
 
-  await transport.sendMail(message);
+    await transport.sendMail(message);
 };
 module.exports = sendEmail;
